faedbf5223198044521ad6dc7dcddcf6347fbf86
76AHMEDFAKHER <220210469@fa-hists.edu.eg>
2026-03-10 16:48:46 +0200

    Complete Week 2: Implemented Serializers, ViewSets, and API routing

--- ecommerce_api/urls.py
+++ ecommerce_api/urls.py
@@ -4,11 +4,24 @@ from rest_framework.routers import DefaultRouter
 from products.views import ProductViewSet, CategoryViewSet
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+
+from django.http import JsonResponse
+
+def api_root(request):
+    return JsonResponse({
+        "message": "Welcome to the E-commerce API",
+        "endpoints": {
+            "products": "/api/products/",
+            "categories": "/api/categories/",
+            "admin": "/admin/"
+        }
+    })
 router = DefaultRouter()
 router.register(r'products', ProductViewSet, basename='product')
 router.register(r'categories', CategoryViewSet, basename='category')
 
 urlpatterns = [
+    path('', api_root),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
